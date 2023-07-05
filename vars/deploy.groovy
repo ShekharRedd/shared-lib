72357d69538d80c8d8d6700587895e0489b408e6
ShekharRedd <115493437+ShekharRedd@users.noreply.github.com>
2023-07-05 12:26:57 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,4 +1,8 @@
 #!/usr/bin/env groovy
-def call(){
+def call(String image){
     echo "deploying the application to production env "
+    withCredentials([usernamePassword(credentialsId: "Docker-hub" , passwordVariable: 'PASS', usernameVariable: 'USER')])
+    sh "docker build -t $image ."
+    sh "echo $PASS | docker login -u $USER --password-stdin"
+    sh "docker push $image"
 }
